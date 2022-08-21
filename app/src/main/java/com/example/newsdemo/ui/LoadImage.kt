10a52064735ec3c34f50694da96eaa3e47c009a2
root <tudoudoudoudoudou@gmail.com>
2022-08-21 14:14:50 +0800

    splash screen commit

--- app/src/main/java/com/example/newsdemo/ui/LoadImage.kt
+++ app/src/main/java/com/example/newsdemo/ui/LoadImage.kt
@@ -4,7 +4,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
-@BindingAdapter("bind:imageUrl")
+@BindingAdapter("imageUrl")
 fun ImageView.setImageUrl(url: String) {
     if (url.isNotEmpty()) {
         Glide.with(this).load(url).into(this)
